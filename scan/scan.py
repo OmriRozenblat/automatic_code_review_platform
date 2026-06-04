a23007a451c337c003f26dc9fc490ef20669687d
Omri <omrirozenb@gmail.com>
2026-06-04 19:37:19 +0300

    get content from path

--- scan/scan.py
+++ scan/scan.py
@@ -9,6 +9,20 @@ class Scan:
             self.rules = f.read().splitlines() 
         self.rules += rules
     
+    def get_file_from_path(self):
+        if not self.path:
+            folder = Path(__file__).parent / "file_to_scan"
+            self.path = next(folder.glob("*.py"), None)
+            if self.path is None:
+                raise FileNotFoundError("No .py file found in file_to_scan")
+
+    
+        try:
+            with open(self.path, "r") as f:
+                self.content = f.read()
+        except FileNotFoundError:
+
+
     def get_rules(self):
         return self.rules
     
@@ -18,6 +32,7 @@ class Scan:
     def add_rule(self, rule: str):
         self.rules.append(rule)
         print("Rule added to scan\n")
+    
     def remove_rule(self, index: int):
         try:
             self.rules.pop(index)
